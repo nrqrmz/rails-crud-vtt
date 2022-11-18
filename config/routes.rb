abb48df80230d4bf2d50e756b96934ba76163e05
Enrique Ramirez <nrqrmz@gmail.com>
2022-11-18 12:18:40 -0600

    implement show action in route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'restaurants#index'
+  
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
 end
