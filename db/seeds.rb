7631066759ae26e64c09f97847553f6496fa3817
Enrique Ramirez <nrqrmz@gmail.com>
2022-11-18 12:09:59 -0600

    seed restaurant model and implement index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+15.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    rating: rand(3..5),
+    address: Faker::Address.street_address
+  )
+end
+
+puts 'restaurants created!'
